cff40d4b9a6cdee7561517cd1c4986ee98ab27cc
mfakyol <55063801+mfakyol@users.noreply.github.com>
2020-09-02 02:25:16 +0300

    random-page-and-cetegory-implemented

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/Home";
 import Detail from "./components/Detail";
+import Random from "./components/Random";
+import Category from "./components/Category";
 
  function App(props) {
   return (
     <div className="App">
       <Router>
       <Navbar/>
-        <Route path="/" exact component={Home} />
+        <Route isAuth={true} path="/" exact component={Home} />
         <Route path="/detail/:id" exact component={Detail} />
+        <Route path="/random" exact component={Random} />
+        <Route path="/category/:category" exact component={Category} />
       </Router>
     </div>
   );
